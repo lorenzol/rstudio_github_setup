fc8a41791a3260a02bac183ae2388e2cff6e3c68
lorenzol <lorenzoluciano1@gmail.com>
2014-09-17 20:32:11 -0400

    first project commit

---
+++ sample_script_1.R
@@ -0,0 +1,8 @@
+
+
+# this is a simple project to show how to setup Rstudio to use Github
+
+# 1) create repository in Github
+# 2) copy repository url
+# 3) create new project in Rstudio using 'Version Control' option
+# 4) copy url in dialog box and other info and that's it
